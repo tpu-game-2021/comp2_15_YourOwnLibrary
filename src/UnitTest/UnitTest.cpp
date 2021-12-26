8a42cbac72f6511561d6d478708b1c799b805a83
2127057 <g2127057@st.t-kougei.ac.jp>
2021-12-26 11:15:53 +0900

    変更しました

--- src/UnitTest/UnitTest.cpp
+++ src/UnitTest/UnitTest.cpp
@@ -19,13 +19,6 @@ namespace UnitTest
 
 		TEST_METHOD(TestMethod2)
 		{
-			int val1 = 0;
-			int val2 = 1;
-			Assert::AreEqual(val1, my_func_int());
-		}
-
-		TEST_METHOD(TestMethod3)
-		{
 			bool val1 = 0;
 			bool val2 = 1;
 			Assert::IsTrue(my_func_bool());
